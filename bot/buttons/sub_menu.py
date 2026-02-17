51fd7a22816e72e21118e7a4c9f8267f0be5f4f0
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-02-17 15:32:09 +0500

    fix

--- bot/buttons/sub_menu.py
+++ bot/buttons/sub_menu.py
@@ -1,8 +1,8 @@
 from aiogram.utils.i18n import gettext as _
 
-MY_REFERRALS = _("My Referrals 📋")
-HELP = _("HELP 📨")
-CATEGORIES = _("Categories 📦")
-SETTINGS = _("Settings ⚙️")
-WELCOME_TEXT = _("Welcome to our bot!😊")
-ADMIN = _("ADMIN🧑‍💼")
+MY_REFERRALS = "My Referrals 📋"
+HELP = "HELP 📨"
+CATEGORIES = "Categories 📦"
+SETTINGS = "Settings ⚙️"
+WELCOME_TEXT = "Welcome to our bot!😊"
+ADMIN = "ADMIN🧑‍💼"
