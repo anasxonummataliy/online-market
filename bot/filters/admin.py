2459cd01ef0df764828116ee50afc3ff57a27f6e
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-03-06 15:50:03 +0500

    last push

--- bot/filters/admin.py
+++ bot/filters/admin.py
@@ -1,9 +1,10 @@
 from aiogram.filters import Filter
+from aiogram.types import Message
 
 from database import User
 
 
 class IsAdmin(Filter):
-    async def __call__(self, message):
+    async def __call__(self, message: Message):
         user = await User.get_user(tg_id=message.from_user.id)
         return user.is_admin
